b9397079783436278c5cf59e0004361292dfc4e1
Kaleem Shaikh <kaleem.shaikh@lr.org>
2024-10-17 03:25:11 +0530

    build and dist

---
+++ frontend/build/precache-manifest.e3293cad874d860707f5ef90dcd9c2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d550350c347a4061dfe46313746c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b892f97f1a023d501791",
+    "url": "/static/css/main.cfdbcc0e.chunk.css"
+  },
+  {
+    "revision": "8f3c7c71cc36253221bc",
+    "url": "/static/js/2.531a5541.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.531a5541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b892f97f1a023d501791",
+    "url": "/static/js/main.c9822052.chunk.js"
+  },
+  {
+    "revision": "59fa1d628e9753c92238",
+    "url": "/static/js/runtime-main.05a35e0b.js"
+  }
+]);
